e418e61ee88ba981e39902c9dbe686b84e4bb8a5
wenxuanr <vcr01123@gmail.com>
2018-04-16 19:26:16 -0400

    change lex

---
+++ handlers/lexBot.js
@@ -0,0 +1,62 @@
+'use strict';
+const AWS = require('aws-sdk');
+const yelp = require('yelp-fusion');
+const config = require('../config');
+const yelpApiKey = config.yelpApi;
+const client = yelp.client(yelpApiKey);
+const lexruntime = new AWS.LexRuntime();
+
+
+function close(sessionAttributes, fulfillmentState, message) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Close',
+            fulfillmentState,
+            message,
+        },
+    };
+}
+
+function delegate(sessionAttributes, slots) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Delegate',
+            slots,
+        },
+    };
+}
+
+
+
+function diningSuggestions(intentRequest, callback) {
+    const location = intentRequest.currentIntent.slots.Location;
+    const cuisine = intentRequest.currentIntent.slots.Cuisine;
+    const number = intentRequest.currentIntent.slots.NumberOfPeople;
+
+    if (source === 'DialogCodeHook'){
+        const outputSessionAttributes = intentRequest.sessionAttributes || {};
+        callback(delegate(outputSessionAttributes, intentRequest.currentIntent.slots));
+        return;
+    }
+
+    callback(close(intentRequest.sessionAttributes, 'Fulfilled',{ contentType: 'PlainText', content: 'You’re all set. Expect my recommendations shortly! Have a good day.' }));
+}
+
+function dispatch(intentRequest, callback) {
+    console.log(`dispatch userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.name}`);
+    const intentName = intentRequest.currentIntent.name;
+    if (intentName === 'SuggestionIntent') {
+        return diningSuggestions(intentRequest, callback);
+    }
+    throw new Error(`Intent with name ${intentName} not supported`);
+}
+
+module.exports.handle = (event, context, callback) => {
+    try {
+        dispatch(event, (response) => callback(null, response));
+    } catch (err) {
+        callback(err);
+    }
+};
