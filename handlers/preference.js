c116f078df15de4c0f38b454f199c03fb56a1aa8
wenxuanr <vcr01123@gmail.com>
2018-05-04 22:36:48 -0400

    change preference

---
+++ handlers/preference.js
@@ -0,0 +1,48 @@
+'use strict';
+const AWS = require('aws-sdk');
+const attr = require('dynamodb-data-types').AttributeValue;
+const dynamodb = new AWS.DynamoDB();
+
+module.exports.handle = (event, context, callback) => {
+    try {
+        // {
+        //     user_id:,
+        //     restaurant_id:,
+        //     like: 0
+        // }
+        const userId = event.user_id;
+        const restaurant_id = event.restaurant_id;
+        const like = event.like;
+
+        let obj = {
+            "userId" : userId,
+            "restaurant_id" : restaurant_id,
+            "like" : like
+        }
+        let datawrap = attr.wrap(obj);
+        let params = {
+            RequestItems: {
+                "restaurant": [{
+                    PutRequest: {
+                        Item: datawrap
+                    }
+                }]
+            }
+        }
+        dynamodb.batchWriteItem(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(data);           // successful response
+            /*
+            data = {
+            }
+            */
+        });
+
+        callback(null, "successully added");
+
+
+
+    } catch (err) {
+        callback(err);
+    }
+};
